a2a562fcaffef55a5e2c6f343875d3791bc9a991
DipuKP2001 <dipukp602@gmail.com>
2020-11-21 13:11:52 +0530

    basic routing and response

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 import { Application } from "https://deno.land/x/oak@v6.3.2/mod.ts"
 
+import { PORT } from './config.js'
+
+import router from './router.js'
+
+const app = new Application()
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+console.log(`Server running on http://localhost:${PORT}`)
+
+await app.listen({ port: PORT })
--- router.js
+++ router.js
@@ -1,1 +1,9 @@
 import { Router } from "https://deno.land/x/oak@v6.3.2/mod.ts"
+
+const router = new Router()
+
+router.get('/',({ response })=>{
+    response.body = "Todo List API using deno runtime"
+})
+
+export default router
